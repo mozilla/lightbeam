1e444bf126b223976f49a7cf123b82cc907fe9a4
Atul Varma <varmaa@gmail.com>
2011-07-11 13:22:22 -0700

    When the addon is installed (i.e. when the page isn't in demo mode), the page is now sized to be the full width of the browser window, rather than just 640 pixels wide. The height of the SVG canvas is also set to be the height of the full page in this case. The page/svg doesn't resize itself when the browser window is dynamically resized, though, so the user currently needs to reload the page manually themselves.
    Also, when the page is reloaded and the addon is installed, the collusion graph properly displays itself.

--- data/force-content-script.js
+++ data/force-content-script.js
@@ -1,19 +1,12 @@
 var graphCallback = null;
-var queuedGraph = null;
 
 unsafeWindow.onGraph = function onGraph(cb) {
   graphCallback = cb;
-  if (queuedGraph) {
-    var graph = queuedGraph;
-    queuedGraph = null;
-    graphCallback(queuedGraph);
-  }
+  self.port.emit("init");
 };
 
 self.port.on("log", function(log) {
   log = JSON.parse(log);
   if (graphCallback)
     graphCallback(log);
-  else
-    queuedGraph = log;
 });
