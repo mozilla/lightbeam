01c6ef32bfe4148a57794acd0ba933a828b9caa4
Atul Varma <varmaa@gmail.com>
2011-07-28 05:58:44 -0400

    window.resetGraph() is now exposed to collusion pages, which resets the state of the collusion graph.

--- data/index-content-script.js
+++ data/index-content-script.js
@@ -5,6 +5,10 @@ unsafeWindow.onGraph = function onGraph(cb) {
   self.port.emit("init");
 };
 
+unsafeWindow.resetGraph = function resetGraph() {
+  self.port.emit('reset');
+};
+
 self.port.on("log", function(log) {
   log = JSON.parse(log);
   if (graphCallback)
