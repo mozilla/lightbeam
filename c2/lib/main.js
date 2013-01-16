d64b59e038d2f855e0d5f8c0cd7d2ec12cf769fe
dethe <dethe@livingcode.org>
2013-01-15 16:53:42 -0800

    tab functionality is working

--- c2/lib/main.js
+++ c2/lib/main.js
@@ -1,6 +1,6 @@
 (function(global) {
     let obSvc = require('observer-service');
-	let Connection = require('connection').Connection;
+    let Connection = require('connection').Connection;
     let tabs = require('tabs');
     let { Cc, Ci, Cr } = require('chrome');
     let winutils = require('api-utils/window/utils');
@@ -16,27 +16,55 @@
     var allConnections = [];
     obSvc.add("http-on-examine-response", function(subject) {
         var connection = new Connection(subject);
-		console.log(connection);
-        allConnections.push(connection);
+        if (connection.valid){
+            // console.log(connection);
+            allConnections.push(connection);
+        }
     });
 
-	tabs.on('activate', function(tab){
-	    getCurrentTab(tab);
-	});
+    tabs.on('activate', function(jptab){
+        var tabinfo = getTabInfo(jptab);
+        var tab = tabinfo.tab;
+        var items = currentConnectionsForTab(tabinfo);
+        // visualize items
+        console.log('this tab matched ' + items.length + ' items');
+    });
 
-	// Tab Listener Component
-	// Iterate through all windows to set tab listeners, then listen for new window
-	var wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
-    function getCurrentTab(jpTab){
-        var currentDomWindow = wm.getMostRecentWindow('navigator:browser');
-        var gBrowser = currentDomWindow.gBrowser;
-        var currentTab = gBrowser.selectedTab;
-        console.log('Current tab: ', Object.keys(currentTab).join(', '));
-        allConnections.forEach(function(connection){
-            if (connection === currentTab){
-                console.log(connection);
-            }
-        });
+    // Tab Listener Component
+    // Iterate through all windows to set tab listeners, then listen for new window
+    var wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+    function getTabInfo(jpTab){
+        // Yeah, this seems to be working! Now I just need to save data sets by tab, and get the load time for the tab
+        var chromeWindow = wm.getMostRecentWindow('navigator:browser');
+        var gBrowser = chromeWindow.gBrowser;
+        var window = gBrowser.contentWindow.wrappedJSObject;
+        return {
+            gBrowser: gBrowser,
+            tab: gBrowser.selectedTab,
+            document: gBrowser.contentDocument,
+            window: window,
+            title: gBrowser.contentTitle, // nsIPrincipal
+            principal: gBrowser.contentPrincipal, // security context
+            uri: gBrowser.contentURI, // nsURI .spec to get string representation
+            loadTime: window.performance.timing.responseStart // milliseconds at which page load was initiated
+        };
+    }
+
+
+    function matchesTab(connection){
+        return connection._sourceTab === this;
+    }
+
+    function matchesCurrentTab(connection){
+        return (connection._sourceTab === this.tab) && (connection.timestamp > this.loadTime);
+    }
+
+    function connectionsForTab(tab){
+        return allConnections.filter(matchesTab, tab);
+    }
+
+    function currentConnectionsForTab(tabinfo){
+        return allConnections.filter(matchesCurrentTab, tabinfo);
     }
 
 
