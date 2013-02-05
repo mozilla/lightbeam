037980072ee0f3cb7b736979e0f03c19c976a492
dethe <dethe@livingcode.org>
2013-02-05 15:38:58 -0800

    updates to clock visualization

--- c2/data/daily.js
+++ c2/data/daily.js
@@ -1,5 +1,8 @@
+// Clock Visualization
+
 // Visualization of tracking data over 24 hours
-(function(){
+
+(function(visualizations){
 "use strict";
 const CX = 300;
 const CY = 500;
@@ -11,6 +14,13 @@ const SVG_NS = 'http://www.w3.org/2000/svg';
 const TIME_X1 = 35;
 const TIME_X2 = 600 - TIME_X1;
 const TIME_Y = CY - 5;
+
+
+// TODO: Make visualization an event emitter, so I can call on('connection', fn) and emit('connection', connection)
+
+var clock = new Emitter();
+visualizations.clock = clock;
+
 
 function svg(name, attrs, text){
     var node = document.createElementNS(SVG_NS, name);
@@ -25,7 +35,7 @@ function svg(name, attrs, text){
     return node;
 }
 
-var visualization = document.querySelector('.visualization');
+var vizcanvas = document.querySelector('.vizcanvas');
 var times = ['12 am', '1 am', '2 am', '3 am', '4 am', '5 am', '6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm', '9 pm', '10 pm', '11 pm', '12 am'];
 var timeslots = {};
 var offsets = [':00', ':15', ':30', ':45'];
@@ -61,14 +71,14 @@ function timeToAngle(date){
 function drawTimes(){
     var text;
     times.slice(0,12).forEach(function(time, idx){
-        visualization.appendChild(svg('text', {
+        vizcanvas.appendChild(svg('text', {
             x: TIME_X1,
             y: TIME_Y,
             transform: 'rotate(' + (7.5 * idx) + ' ' + CENTRE + ') ' + TIME_TRANS
         }, time)
     );});
     times.slice(12).reverse().forEach(function(time, idx){
-        visualization.appendChild(svg('text', {
+        vizcanvas.appendChild(svg('text', {
             x: TIME_X2,
             y: TIME_Y,
             transform: 'rotate(' + (-7.5 * idx) + ' ' + CENTRE + ') ' + TIME_TRANS
@@ -76,32 +86,36 @@ function drawTimes(){
     )});
 }
 
-function drawTrackerDots(){
-    var count = 0;
-    times.forEach(function(time, idx){
-        var trackers, angle, group, circle;
-        offsets.forEach(function(offset, offIdx){
-            trackers = timeslots[time][offset];
-            if (!trackers.length) return;
-            count += trackers.length;
-            angle = -180 + (idx * 4 + offIdx) * 1.875; // in degrees
-            // <g transform="rotate(-46.875 300,500) translate(640,495)">
-            group = document.createElementNS(SVG_NS, 'g');
-            group.setAttribute('transform', 'rotate(' + angle + ' ' + CENTRE + ') ' + DOT_TRANS );
-            trackers.forEach(function(tracker, trackIdx){
-                // <circle cx="0" cy="0" r="3" class="tracker" />
-                circle = document.createElementNS(SVG_NS, 'circle');
-                circle.setAttribute('cx', trackIdx * 10);
-                circle.setAttribute('cy', '0');
-                circle.setAttribute('r', '3');
-                circle.setAttribute('class', 'tracker');
-                circle.setAttribute('title', tracker.target);
-                group.appendChild(circle);
-            });
-            visualization.appendChild(group);
-        });
-    });
-}
+clock.timeslots = new Array(24);
+
+// TODO: Use real timestamps for test data
+// TODO: implement timeToBucket
+
+clock.on('connection', function(connection){
+    // A connection has the following keys:
+    // source (url), target (url), timestamp (int), contentType (str), cookie (bool), sourceVisited (bool), secure(bool), sourcePathDepth (int), sourceQueryDepth(int)
+    console.log('clock connection');
+    var bucketIdx = timeToBucket(connection.timestamp, 96);
+    if (! clock.timeslots[bucketIdx]){
+        var angle = -180 + (bucketidx * 1.875); // in degrees
+        clock.timeslots[bucketIdx] = {
+            group: svg('g', {
+                transform: 'rotate(' + angle + ' ' + CENTRE + ') ' + DOT_TRANS
+            }),
+            connections: []
+        }
+        vizcanvas.appendChild(clock.timeslots[bucketIdx].group);
+    }
+    var bucket = clock.timeslots[bucketIdx];
+    var connectionIdx = bucket.connections.length;
+    connections.push(connection);
+    bucket.group.appendChild(svg('circle', {
+        cx: connectionIdx * 10,
+        cy: 0,
+        r: 3,
+        'class': 'tracker'
+    }));
+});
 
 function drawTimerHand(time){
     if (!time) time = new Date();
@@ -110,44 +124,20 @@ function drawTimerHand(time){
         var hand = svg('g', {id: 'timerhand'});
         hand.appendChild(svg('line', {x1: 0, y1: 0, x2: 400, y2: 0}));
         hand.appendChild(svg('path', {d: 'M47,-8 L47,8 73,5 73,-5 Z'}));
-        visualization.appendChild(hand);
+        vizcanvas.appendChild(hand);
     }
     hand.setAttribute('transform', 'rotate(' + (timeToAngle(time) - 180) + ' ' + CENTRE + ') ' + HAND_TRANS);
-    console.log('hand: %o', hand);
+    setTimeout(drawTimerHand, 1000);
 }
 
-function update(){
+
+clock.on('init', function(connections){
     // draw clock dial
     drawTimes();
-    drawTrackerDots();
     drawTimerHand();
-    // draw text info
-}
+    console.log('clock init');
+});
 
-function test(){
-    var urls = ['google.com', 'adsense.google.com', 'youtube.com', 'friendfeed.com', 'news.google.com', 'mozilla.org', 'apple.com', 'twitter.com', 'facebook.com', 'news.google.com', 'nytimes.com', 'boingboing.net', 'slashdot.org', 'livingcode.org', 'marginalrevolution.org'];
-    var times = ['8:11 am', '8:31 am', '8:47 am', '9:01 am', '9:15 am', '10:20 am', '10:46 am', '11:00 am', '11:20 am', '11:35 am', '1:15 pm', '1:31 pm', '2:00 pm', '2:15 pm', '2:30 pm', '2:45 pm', '3:00 pm', '3:45 pm', '4:00 pm', '4:45 pm', '5:00 pm', '5:16 pm', '7:45 pm', '8:00 pm', '8:30 pm', '8:45 pm', '10:15 pm', '10:30 pm'];
-    function addTestTracker(date, time, url){
-        var t = time.split(':');
-        var h = parseInt(t[0], 10) - 1;
-        if (time.split(' ')[1] === 'pm') h += 12;
-        date.setHours(h);
-        date.setMinutes(parseInt(t[1], 10));
-        addTracker({target: url, timestamp: date.valueOf()});
-    }
-    var today = new Date();
-    times.forEach(function(time, idx){
-        var count = urls[idx % urls.length].length;
-        for (var i = 0; i < count; i++){
-            addTestTracker(today, time, urls[(idx + i) % urls.length]);
-        }
-    });
-    update();
-}
-test();
+})(visualizations);
 
-window.vis = window.vis || {};
-vis.addTracker = addTracker;
-vis.update = update;
-
-})();
+console.log('clock visualization initialized');
