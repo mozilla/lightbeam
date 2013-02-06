bdcb95a3e19356162499f62026f925a97a173197
dethe <dethe@livingcode.org>
2013-02-06 14:46:59 -0800

    implement data export in ui

--- c2/data/ui.js
+++ c2/data/ui.js
@@ -10,29 +10,29 @@
 
 window.addEventListener("DOMContentLoaded", function(){
     var sampleDropdown = document.querySelector(".btn_group");
-    
+
     /* dropdown lists on the side bar */
     function dropDownGroup(btnGroup, callback){
         var view = btnGroup.querySelector("[data-view]");
         var list = btnGroup.querySelector("[data-list]");
-    
+
         callback = callback || function(){};
-    
+
         btnGroup.addEventListener("click", function(e){
             var selected = btnGroup.querySelector("[data-selected]");
             var targetValue = e.target.getAttribute("data-value");
             var activeDropdown = document.querySelector(".active_dropdown");
-            
+
             // allows only one open dropdown list at a time
             if( activeDropdown && !btnGroup.classList.contains("active_dropdown") ){
                 activeDropdown.querySelector(".dropdown_options").classList.add("collapsed");
                 activeDropdown.classList.remove("active_dropdown");
             }
-            
+
             // opens up the current selected dropdown list
             btnGroup.querySelector(".dropdown_options").classList.toggle("collapsed");
             btnGroup.classList.toggle("active_dropdown");
-            
+
             // when user selects an option from the dropdown list
             if ( targetValue ){
                 view.querySelector("a:not(.invi_focus)").innerHTML = e.target.innerHTML;
@@ -40,11 +40,11 @@ window.addEventListener("DOMContentLoaded", function(){
                 e.target.setAttribute("data-selected", true);
                 callback(targetValue);
             }
-            
+
         }, false);
     }
-    
-    
+
+
     /* bind actions to each of the btn_group elements */
     [].forEach.call(
         document.querySelectorAll(".btn_group"),
@@ -54,7 +54,7 @@ window.addEventListener("DOMContentLoaded", function(){
             });
         }
     )
-                        
+
 
     /* Toggle Info Panel */
     document.querySelector(".temp_showinfo").addEventListener("click", function(){
@@ -64,6 +64,13 @@ window.addEventListener("DOMContentLoaded", function(){
 
 });
 
+document.querySelector(".download").addEventListener('click', function() {
+    addon.once('export-data', function(connections){
+        console.log('received export data');
+        window.open('data:application/json,' + connections);
+    });
+    addon.emit('export');
+});
 
 
 
