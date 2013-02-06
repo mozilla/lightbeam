14d355d6e6a2ae3358d46ed853a5aed24c999128
dethe <dethe@livingcode.org>
2013-02-06 14:43:14 -0800

    fix event forwarding from addon to ui

--- c2/data/content-script.js
+++ c2/data/content-script.js
@@ -1,32 +1,31 @@
-var graphCallback = null;
-
-function init(cb) {
-  graphCallback = cb;
-  self.port.emit("init");
-};
-
-/* resetGraph effectively wipes out the graph in storage
- * because after it is called, an empty graph is passed
- * to 'self.port.on("log")'.
- */
-function reset() {
-  self.port.emit('reset');
-};
-
-function save() {
-  self.port.emit('save');
-};
-
-self.port.on("log", function(log) {
-  log = JSON.parse(log);
-  if (graphCallback) {
-    self.port.emit('save', JSON.stringify(log));
-    graphCallback(log);
-  }
+self.port.on('log', function log(arguments){
+    if (unsafeWindow && unsafeWindow.console){
+        unsafeWindow.console.log.call(unsafeWindow, arguments);
+    }else{
+        console.log('cannot call browser logging: ' + unsafeWindow);
+    }
 });
 
-unsafeWindow.Collusion = {
-  init: init,
-  reset: reset,
-  save: save
-};
+self.port.on('connection', function(connection){
+    if (unsafeWindow && unsafeWindow.currentVisualization){
+        // var connection = JSON.parse(message);
+        connection.timestamp = new Date(connection.timestamp);
+        unsafeWindow.currentVisualization.emit('connection', connection);
+    }else{
+        console.log('cannot call unsafeWindow.currentVisualization: '  + unsafeWindow);
+    }
+});
+
+self.port.on('init', function(message){
+    if (unsafeWindow && unsafeWindow.currentVisualization){
+        var connections = message.map(function(connection){
+            connection.timestamp = new Date(connection.timestamp);
+            return connection;
+        });
+        unsafeWindow.currentVisualization.emit('init', connections);
+    }else{
+        console.log('cannot call unsafeWindow.currentVisualization: ' + unsafeWindow);
+    }
+});
+
+unsafeWindow.addon = self.port;
