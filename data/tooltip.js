b1e897105d707edc4cce0e857a1c6e359865e913
dethe <dethe@livingcode.org>
2013-02-26 17:16:07 -0800

    patched persistence, should be much faster now

--- data/tooltip.js
+++ data/tooltip.js
@@ -3,7 +3,6 @@
 var tooltipTimer;
 
 function showTooltip(event){
-    // console.log('Show tooltip for %s: %s', event.target.tagName, event.target.getAttribute('data-target'));
     var tooltip = document.getElementById('tooltip');
     tooltip.style.left = '-1000px';
     tooltip.style.display = 'inline-block';
@@ -12,13 +11,8 @@ function showTooltip(event){
     tooltip.innerHTML = d.source + ' -> ' + d.target + '<br />' + d.timestamp + ' (&times;' + d.howMany + ')';
     var rect = event.target.getClientRects()[0];
     var tooltipWidth = tooltip.offsetWidth;
-    // console.log('rect: %o, width: %s', rect, tooltipWidth);
     tooltip.style.top = (rect.top - 60) + 'px';
     tooltip.style.left = (rect.left + (rect.width / 2) - (tooltipWidth / 2)) + 'px';
-    // PLACEHOLDER (FOR DEBUGGING)
-    // placeholder.style.left = rect.left + 'px';
-    // placeholder.style.top = rect.top + 'px';
-    // placeholder.style.border = '1px solid red';
     return false;
 }
 
