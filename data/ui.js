f34c7dc70ce59b3085069bdcc8a47bbd2aaf01d0
Mavis Ou <mmmavisss@gmail.com>
2013-03-19 16:29:52 -0700

    center the highlight country on map

--- data/ui.js
+++ data/ui.js
@@ -85,76 +85,154 @@ document.querySelector('.upload').addEventListener('click', function(){
     addon.emit('upload');
 });
 
-function getZoom(){
-    var box = document.querySelector('.vizcanvas')
-        .getAttribute('viewBox')
-        .split(/\s/)
-        .map(function(i){ return parseInt(i, 10); });
-    return {x: box[0], y: box[1], w: box[2], h: box[3]};
+function getZoom(canvas){
+    // TODO: code cleanup if both cases use basically the same code
+    switch(canvas){
+        case 'vizcanvas': {
+            var box = document.querySelector('.vizcanvas')
+                        .getAttribute('viewBox')
+                        .split(/\s/)
+                        .map(function(i){ return parseInt(i, 10); });
+            return {x: box[0], y: box[1], w: box[2], h: box[3]};
+        }
+        case 'mapcanvas': {
+            var box = document.querySelector('#mapcanvas')
+                        .getAttribute('viewBox')
+                        .split(/\s/)
+                        .map(function(i){ return parseInt(i, 10); });
+            console.log(box);
+            return {x: box[0], y: box[1], w: box[2], h: box[3]};
+        }
+        default: throw new Error('It has to be one of the choices above');
+    }
 }
 
-function setZoom(box){
-    document.querySelector('.vizcanvas')
-        .setAttribute('viewBox', [box.x, box.y, box.w, box.h].join(' '));
+function setZoom(box,canvas){
+    // TODO: code cleanup if both cases use basically the same code
+    switch(canvas){
+        case 'vizcanvas': {
+                document.querySelector('.vizcanvas')
+                    .setAttribute('viewBox', [box.x, box.y, box.w, box.h].join(' '));
+                break;
+        }
+        case 'mapcanvas': {
+                document.querySelector('#mapcanvas')
+                    .setAttribute('viewBox', [box.x, box.y, box.w, box.h].join(' '));
+                break;
+                
+        }
+        default: throw new Error('It has to be one of the choices above');
+    }
 }
 
-document.querySelector('.move-up').addEventListener('click', function(){
-    var box = getZoom();
+document.querySelector('.controls_options .move-up').addEventListener('click', function(){
+    var box = getZoom('vizcanvas');
     var dY = Math.floor(box.h / 10);
     box.y += dY;
-    setZoom(box);
+    setZoom(box,'vizcanvas');
     return false;
 });
 
-document.querySelector('.move-down').addEventListener('click', function(){
-    var box = getZoom();
+document.querySelector('.controls_options .move-down').addEventListener('click', function(){
+    var box = getZoom('vizcanvas');
     var dY = Math.floor(box.h / 10);
     box.y -= dY;
-    setZoom(box);
+    setZoom(box,'vizcanvas');
     return false;
 });
 
-document.querySelector('.move-left').addEventListener('click', function(){
-    var box = getZoom();
+document.querySelector('.controls_options .move-left').addEventListener('click', function(){
+    var box = getZoom('vizcanvas');
     var dX = Math.floor(box.w / 10);
     box.x += dX;
-    setZoom(box);
+    setZoom(box,'vizcanvas');
     return false;
 });
 
-document.querySelector('.move-right').addEventListener('click', function(){
-    var box = getZoom();
+document.querySelector('.controls_options .move-right').addEventListener('click', function(){
+    var box = getZoom('vizcanvas');
     var dX = Math.floor(box.w / 10);
     box.x -= dX;
-    setZoom(box);
+    setZoom(box,'vizcanvas');
     return false;
 });
 
-document.querySelector('.zoom-in').addEventListener('click', function(){
-    var box = getZoom();
+document.querySelector('.controls_options .zoom-in').addEventListener('click', function(){
+    var box = getZoom('vizcanvas');
     var dX = Math.floor(box.w / 5);
     var dY = Math.floor(box.h / 5);
     // box.x -= dX;
     // box.h -= dY;
     box.w /= 1.1;
     box.h /= 1.1;
-    setZoom(box);
+    setZoom(box,'vizcanvas');
     return false;
 });
 
-document.querySelector('.zoom-out').addEventListener('click', function(){
-    var box = getZoom();
+document.querySelector('.controls_options .zoom-out').addEventListener('click', function(){
+    var box = getZoom('vizcanvas');
     var dX = Math.floor(box.w / 5);
     var dY = Math.floor(box.h / 5);
     // box.x += dX;
     // box.h += dY;
     box.w *= 1.1;
     box.h *= 1.1;
-    setZoom(box);
+    setZoom(box,'vizcanvas');
     return false;
 });
 
 
+/* Map Controls ========================= */
+
+document.querySelector('.map-control .move-up').addEventListener('click', function(){
+    var box = getZoom('mapcanvas');
+    var dY = Math.floor(box.h / 10);
+    box.y += dY;
+    setZoom(box,'mapcanvas');
+    return false;
+});
+
+document.querySelector('.map-control .move-down').addEventListener('click', function(){
+    var box = getZoom('mapcanvas');
+    var dY = Math.floor(box.h / 10);
+    box.y -= dY;
+    setZoom(box,'mapcanvas');
+    return false;
+});
+
+document.querySelector('.map-control .move-left').addEventListener('click', function(){
+    var box = getZoom('mapcanvas');
+    var dX = Math.floor(box.w / 10);
+    box.x += dX;
+    setZoom(box,'mapcanvas');
+    return false;
+});
+
+document.querySelector('.map-control .move-right').addEventListener('click', function(){
+    var box = getZoom('mapcanvas');
+    var dX = Math.floor(box.w / 10);
+    box.x -= dX;
+    setZoom(box,'mapcanvas');
+    return false;
+});
+
+document.querySelector('.map-control .zoom-in').addEventListener('click', function(){
+    // TODO: zoom in/out adjustment
+    var box = getZoom('mapcanvas');
+    box.w /= 1.5;
+    box.h /= 1.5;
+    setZoom(box,'mapcanvas');
+    return false;
+});
+
+document.querySelector('.map-control .zoom-out').addEventListener('click', function(){
+    // TODO: zoom in/out adjustment
+    var box = getZoom('mapcanvas');
+    box.w *= 1.5;
+    box.h *= 1.5;
+    setZoom(box,'mapcanvas');
+    return false;
+});
 
 
 
