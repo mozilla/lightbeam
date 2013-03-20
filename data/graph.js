0be9250ce7f9878e1353437853085bbf445d66db
dethe <dethe@livingcode.org>
2013-03-19 18:56:41 -0700

    moved aggregate data out of graph for re-use

--- data/graph.js
+++ data/graph.js
@@ -11,20 +11,6 @@ visualizations.graph = graph;
 var width = 1000, height = 1000;
 var force, vizcanvas, vis;
 
-// Should we separate source nodes and target nodes?
-var nodemap, allnodes, sitenodes, thirdnodes, bothnodes, edgemap, edges;
-
-function resetData(){
-    allnodes = [];
-    nodemap = {};
-    sitenodes = [];
-    thirdnodes = [];
-    bothnodes = [];
-    edgemap = {};
-    edges = [];
-}
-resetData();
-
 // There are three phases for a visualization life-cycle:
 // init does initialization and receives the existing set of connections
 // connection notifies of a new connection that matches existing filter
@@ -38,85 +24,22 @@ function onInit(connections){
     vizcanvas = document.querySelector('.vizcanvas');
     vis = d3.select('.vizcanvas');
     // A D3 visualization has a two main components, data-shaping, and setting up the D3 callbacks
-    connections.forEach(function(connection){
-        // This does our data shaping and is also used by visualization.on('connect')
-        onConnection(connection);
-    });
+    aggregate.emit('load', connections);
     // This binds our data to the D3 visualization and sets up the callbacks
     initGraph();
+    aggregate.on('updated', function(){
+        // new nodes, reheat graph simulation
+        if (force){
+            force.start();
+        }
+    });
     // Differenct visualizations may have different viewBoxes, so make sure we use the right one
     vizcanvas.setAttribute('viewBox', [0,0,width,height].join(' '));
 };
 
 function onConnection(connection){
-    // A connection has the following keys:
-    // source (url), target (url), timestamp (int), contentType (str), cookie (bool), sourceVisited (bool), secure(bool), sourcePathDepth (int), sourceQueryDepth(int)
-    // We want to shape the collection of connections that represent points in time into
-    // aggregate data for graphing. Each connection has two endpoints represented by GraphNode objects
-    // and one edge represented by a GraphEdge object, but we want to re-use these when connections
-    // map to the same endpoints or edges.
-    var sourcenode, targetnode, edge, nodelist, reheat = false;
-    if (nodemap[connection.source]){
-        sourcenode = nodemap[connection.source];
-        var oldNodeType = sourcenode.nodeType;
-        sourcenode.update(connection, true);
-        if (oldNodeType !== sourcenode.nodeType){
-            moveNode(sourcenode, oldNodeType);
-            reheat = true;
-	}
-    }else{
-        sourcenode = new GraphNode(connection, true);
-        nodemap[connection.source] = sourcenode;
-        nodelist = getNodeList(sourcenode.nodeType);
-        nodelist.push(sourcenode);
-	allnodes.push(sourcenode);
-        reheat = true;
-    }
-    if (nodemap[connection.target]){
-        targetnode = nodemap[connection.target];
-        var oldNodeType = targetnode.nodeType;
-        targetnode.update(connection, false);
-        if (oldNodeType !== targetnode.nodeType){
-            moveNode(targetnode, oldNodeType);
-            reheat = true;
-	}
-    }else{
-        targetnode = new GraphNode(connection, false);
-        nodemap[connection.target] = targetnode;
-        nodelist = getNodeList(targetnode.nodeType);
-        nodelist.push(targetnode);
-        allnodes.push(targetnode); // all nodes
-        reheat = true
-    }
-    if (edgemap[connection.source + '->' + connection.target]){
-        edge = edgemap[connection.source + '->' + connection.target];
-    }else{
-        edge = new GraphEdge(sourcenode, targetnode);
-        edgemap[edge.name] = edge;
-        edges.push(edge);
-        reheat = true;
-    }
-    if (force && reheat){ // reheat is default, pass false in to turn it off
-        force.start();
-    }
+    aggregate.emit('connection', connection);
 }
-
-function getNodeList(nodeType){
-    switch(nodeType){
-        case 'site': return sitenodes;
-        case 'thirdparty': return thirdnodes;
-        case 'both': return bothnodes;
-        default: throw new Error('It has to be one of the choices above');
-    }
-}
-
-function moveNode(node, oldNodeType){
-    var oldlist = getNodeList(oldNodeType);
-    var newlist = getNodeList(node.nodeType);
-    oldlist.splice(oldlist.indexOf(node), 1);
-    newlist.push(node);
-}
-
 
 function onRemove(){
     console.log('removing graph');
@@ -124,9 +47,12 @@ function onRemove(){
         force.stop();
         force = null;
     }
-    resetData();
+    aggregate.emit('reset');
     resetCanvas();
 };
+
+
+// UTILITIES
 
 function point(angle, size){
 	return [Math.round(Math.cos(angle) * size), -Math.round(Math.sin(angle) * size)];
@@ -146,19 +72,20 @@ function polygonAsString(points, size){
     return poly.map(function(pair){return pair.join(',');}).join(' ');
 }
 
+// SET UP D3 HANDLERS
 
 function initGraph(){
     // Initialize D3 layout and bind data
     force = d3.layout.force()
-        .nodes(allnodes)
-        .links(edges)
+        .nodes(aggregate.allnodes)
+        .links(aggregate.edges)
         .charge(-500)
         .size([width,height])
         .start();
 
         // Data binding for links
         var lines = vis.selectAll('.edge')
-            .data(edges, function(edge){ return edge.name; });
+            .data(aggregate.edges, function(edge){ return edge.name; });
 
         lines.enter()
             .insert('line', ':first-child')
@@ -167,17 +94,18 @@ function initGraph(){
         lines.exit()
             .remove();
 
-        var nodes = vis.selectAll('.node')
-	    .data(allnodes, function(node){ return node.name; })
-        .call(force.drag);
+ //        var nodes = vis.selectAll('.node')
+	//     .data(aggregate.allnodes, function(node){ return node.name; })
+ //        .call(force.drag);
 
-	nodes.enter();
+	// nodes.enter();
 
-        nodes.exit()
-            .remove();
+ //        nodes.exit()
+ //            .remove();
 
         var sites = vis.selectAll('.site')
-            .data(sitenodes, function(node){ return node.name; });
+            .data(aggregate.sitenodes, function(node){ return node.name; })
+            .call(force.drag);
 
         sites.enter()
             .append('circle')
@@ -191,7 +119,8 @@ function initGraph(){
             .classed('site', true);
 
         var thirdparties = vis.selectAll('.thirdparty')
-            .data(thirdnodes, function(node){ return node.name; });
+            .data(aggregate.thirdnodes, function(node){ return node.name; })
+            .call(force.drag);
 
         thirdparties.enter()
             .append('polygon')
@@ -203,7 +132,8 @@ function initGraph(){
             .classed('thirdparty', true);
 
         var boths = vis.selectAll('.both')
-            .data(bothnodes, function(node){ return node.name; });
+            .data(aggregate.bothnodes, function(node){ return node.name; })
+            .call(force.drag);
 
         boths.enter()
     	    .append('rect')
@@ -246,80 +176,6 @@ function updateNodes(thenodes){
 }
 
 
-function GraphEdge(source, target){
-    this.source = source;
-    this.target = target;
-    this.name = source.name + '->' + target.name;
-}
-GraphEdge.prototype.lastAccess = function(){
-    return (this.source.lastAccess > this.target.lastAccess) ? this.source.lastAccess : this.target.lastAccess;
-}
-GraphEdge.prototype.firstAccess = function(){
-    return (this.source.firstAccess < this.target.firstAccess) ? this.source.firstAccess : this.target.firstAccess;
-}
-
-// A graph node represents one end of a connection, either a target or a source
-// Where a connection is a point in time with a timestamp, a graph node has a  time range
-// represented by firstAccess and lastAccess. Where a connection has a contentType, a node
-// has an array of content types. Booleans in graph nodes become boolean pairs in graph nodes
-// (for instance, some connections may have cookies and some may not, which would result in both
-// cookie and notCookie being true). We set an initial position randomly to keep the force graph
-// from exploding.
-//
-// FIXME: Other visualizations could use this aggregate structure for reporting. Move it out of
-// the graph visualization into a re-usable library.
-function GraphNode(connection, isSource){
-    this.firstAccess = this.lastAccess = connection.timestamp;
-    this.linkedFrom = [];
-    this.linkedTo = [];
-    this.contentTypes = [];
-    this.cookie = false;
-    this.notCookie = false;
-    this.visited = false;
-    this.notVisited = false;
-    this.secure = false;
-    this.notSecure = false;
-    this.howMany = 0;
-    if (connection){
-        this.update(connection, isSource);
-    }
-}
-GraphNode.prototype.update = function(connection, isSource){
-    if (!this.name){
-        this.name = isSource ? connection.source : connection.target;
-    }
-    if (connection.timestamp > this.lastAccess){
-        this.lastAccess = connection.timestamp;
-    }
-    if (connection.timestamp < this.firstAccess){
-        this.firstAccess = connection.timestamp;
-    }
-    if (isSource && (this.linkedTo.indexOf(connection.target) < 0)){
-        this.linkedTo.push(connection.target);
-    }
-    if ((!isSource) && (this.linkedFrom.indexOf(connection.source) < 0)){
-        this.linkedFrom.push(connection.source);
-    }
-    if (this.contentTypes.indexOf(connection.contentType) < 0){
-        this.contentTypes.push(connection.contentType);
-    }
-    this.cookie = this.cookie || connection.cookie;
-    this.notCookie = this.notCookie || (!connection.cookie);
-    this.visited = this.visited || connection.sourceVisited;
-    this.notVisited = this.notVisited || (!connection.sourceVisited);
-    if (this.visited && this.notVisited){
-        this.nodeType = 'both';
-    }else if (this.visited){
-        this.nodeType = 'site';
-    }else{
-	this.nodeType = 'thirdparty';
-    }
-    this.secure = this.secure || connection.secure;
-    this.notSecure = this.secure || (!connection.visited);
-    this.howMany++;
-    return this;
-};
-
 
 // FIXME: Move this out of visualization so multiple visualizations can use it.
 function resetCanvas(){
@@ -330,96 +186,5 @@ function resetCanvas(){
     vizcanvas = newcanvas;
 }
 
-
-// update info
-document.querySelector('#content').addEventListener('click', function(event){
-    if (event.target.mozMatchesSelector('.node')){
-        // var preHighlight = document.querySelectorAll(".highlight-country");
-//         if (preHighlight){
-//             toArray(preHighlight).forEach(function(element){
-//                 element.classList.remove("highlight-country");
-//             });
-//         }
-
-        updateInfo(nodemap[event.target.getAttribute('data-name')]);
-    }
-});
-
-/* Updates info on the right info bar */
-function updateInfo(node){
-    function getServerInfo(theUrl){
-        var xmlHttp = null;
-        xmlHttp = new XMLHttpRequest();
-        xmlHttp.open( "GET", theUrl, false );
-        xmlHttp.send( null );
-        return (xmlHttp.status == 200) ? JSON.parse(xmlHttp.responseText) : false;
-    }
- 
-    function resetMap(){
-        var preHighlight = document.querySelectorAll(".highlight-country");
-        if (preHighlight){
-            toArray(preHighlight).forEach(function(element){
-                element.classList.remove("highlight-country");
-            });
-        }
-        document.querySelector("#mapcanvas").setAttribute("viewBox", [0,81.5,2711.3,1196.7].join(" "));
-    }
-
-    var nodeName = node.name;
-    document.querySelector(".holder .title").innerHTML = nodeName;
-    document.querySelector(".holder .url").innerHTML = nodeName;  
-    var info = parseUri(nodeName); // uses Steven Levithan's parseUri 1.2.2
-    var jsonURL = "http://freegeoip.net/json/" + info.host;
-    var data = getServerInfo(jsonURL);
-
-    if ( data == false ){
-        document.querySelector("#country").innerHTML = "(Cannot find server location)";
-        resetMap();
-    }else{
-        // update country info only when it is different from the current one
-        if ( data.country_name !==  document.querySelector("#country").innerHTML ){
-            resetMap();
-            document.querySelector("#country").innerHTML = data.country_name;
-            var countryOnMap = document.querySelectorAll("svg ." + data.country_code.toLowerCase() + " > *");
-            if ( countryOnMap ){
-                toArray(countryOnMap).forEach(function(path){
-                    path.classList.add("highlight-country");
-                });
-            }
-
-            // position the highlighted country in center
-            var svgViewBox = document.querySelector("#mapcanvas").getAttribute("viewBox").split(" ");
-            var worldDimen = document.querySelector("#mapcanvas").getClientRects()[0];
-            var countryDimen = document.querySelector("#"+data.country_code.toLowerCase()).getClientRects()[0];
- 
-            var ratio = svgViewBox[2] / worldDimen.width;
-            var worldCenter = {
-                x: 0.5*worldDimen.width+worldDimen.left,
-                y: 0.5*worldDimen.height+worldDimen.top
-            };
-            var countryCenter = {
-                x: 0.5*countryDimen.width+countryDimen.left,
-                y: 0.5*countryDimen.height+countryDimen.top
-            };
-            document.querySelector("#mapcanvas").setAttribute("viewBox",
-                            [ -(worldCenter.x-countryCenter.x) * ratio,
-                              -(worldCenter.y-countryCenter.y) * ratio,
-                              svgViewBox[2],
-                              svgViewBox[3]]
-                            .join(" ")); 
-        }
-    }
-
-    // update the connections list
-    var connections = new Array();
-    var htmlList = "";
-    connections = connections.concat(node.linkedFrom, node.linkedTo);
-    connections.forEach(function(conn){
-        htmlList = htmlList + "<li>" + conn + "</li>";
-    });
-    document.querySelector(".connections-list").querySelector(".blue-text").innerHTML = connections.length + " connections from current site";
-    document.querySelector(".connections-list ul").innerHTML = htmlList;
-
-}
 
 })(visualizations);
