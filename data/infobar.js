5feea8c7d537487d7ed18eaf43ed1b2b946d87e7
Mavis Ou <mmmavisss@gmail.com>
2013-03-21 16:16:41 -0700

    adding zoom/pan handlers; code cleanup

--- data/infobar.js
+++ data/infobar.js
@@ -7,61 +7,67 @@ document.querySelector('#content').addEventListener('click', function(event){
     }
 },false);
 
+
+// get server info from http://freegeoip.net
+function getServerInfo(node, callback){
+    var info = parseUri(node.name); // uses Steven Levithan's parseUri 1.2.2
+    var jsonURL = "http://freegeoip.net/json/" + info.host;
+    var xmlHttp = null;
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", jsonURL, false );
+    xmlHttp.send( null );
+    callback( (xmlHttp.status == 200) ? JSON.parse(xmlHttp.responseText) : false );
+}
+
+// reset map
+function resetMap(){
+    var preHighlight = document.querySelectorAll(".highlight-country");
+    if (preHighlight){
+        toArray(preHighlight).forEach(function(element){
+            element.classList.remove("highlight-country");
+        });
+    }
+    document.querySelector("#mapcanvas").setAttribute("viewBox", [0,0,2711.3,1196.7].join(" "));
+    d3.selectAll("#mapcanvas > *").attr("transform","translate(0,0)  scale(1.0)");
+}
+
+// update map 
+function updateMap(countryCode){
+    var countryOnMap = d3.select("#mapcanvas").select("#" + countryCode.toLowerCase());
+    countryOnMap.classed("highlight-country", true);
+    countryOnMap.selectAll("*").classed("highlight-country", true);
+
+    // position the highlighted country in center
+    var svgViewBox = document.querySelector("#mapcanvas").getAttribute("viewBox").split(" ");
+    var worldDimen = document.querySelector("#mapcanvas").getClientRects()[0];
+    var countryDimen = document.querySelector("#"+countryCode).getClientRects()[0];
+
+    var ratio = svgViewBox[2] / worldDimen.width;
+    var worldCenter = {
+        x: 0.5*worldDimen.width + worldDimen.left,
+        y: 0.5*worldDimen.height + worldDimen.top
+    };
+    var countryCenter = {
+        x: 0.5*countryDimen.width + countryDimen.left,
+        y: 0.5*countryDimen.height + countryDimen.top
+    };
+
+    var newViewBox = {
+        x: (countryCenter.x-worldCenter.x) * ratio,
+        y: (countryCenter.y-worldCenter.y) * ratio,
+        w: svgViewBox[2],
+        h: svgViewBox[3]
+    };
+    setZoom(newViewBox,'mapcanvas');
+}
+
+
+
 // updates info on the right info bar 
 function updateInfo(node){
 
-    function getServerInfo(theUrl, callback){
-        var xmlHttp = null;
-        xmlHttp = new XMLHttpRequest();
-        xmlHttp.open( "GET", theUrl, false );
-        xmlHttp.send( null );
-        callback( (xmlHttp.status == 200) ? JSON.parse(xmlHttp.responseText) : false );
-    }
-
-    function resetMap(){
-        var preHighlight = document.querySelectorAll(".highlight-country");
-        if (preHighlight){
-            toArray(preHighlight).forEach(function(element){
-                element.classList.remove("highlight-country");
-            });
-        }
-        document.querySelector("#mapcanvas").setAttribute("viewBox", [0,0,2711.3,1196.7].join(" "));
-    }
-
-    function updateMap(newCountry, countryCode){
-        toArray(newCountry).forEach(function(land){
-            land.classList.add("highlight-country");
-        });
- 
-        // position the highlighted country in center
-        var svgViewBox = document.querySelector("#mapcanvas").getAttribute("viewBox").split(" ");
-        var worldDimen = document.querySelector("#mapcanvas").getClientRects()[0];
-        var countryDimen = document.querySelector("#"+countryCode).getClientRects()[0];
-
-        var ratio = svgViewBox[2] / worldDimen.width;
-        var worldCenter = {
-            x: 0.5*worldDimen.width + worldDimen.left,
-            y: 0.5*worldDimen.height + worldDimen.top
-        };
-        var countryCenter = {
-            x: 0.5*countryDimen.width + countryDimen.left,
-            y: 0.5*countryDimen.height + countryDimen.top
-        };
-
-        var newViewBox = {
-            x: -(worldCenter.x-countryCenter.x) * ratio,
-            y: -(worldCenter.y-countryCenter.y) * ratio,
-            w: svgViewBox[2],
-            h: svgViewBox[3]
-        };
-        setZoom(newViewBox,'mapcanvas');
-    }
-
-    var info = parseUri(node.name); // uses Steven Levithan's parseUri 1.2.2
-    var jsonURL = "http://freegeoip.net/json/" + info.host;
-
-    // update content in the side bar when you have the server info ==========
-    getServerInfo(jsonURL, function(data){
+    // update content in the side bar 
+    getServerInfo(node, function(data){
         document.querySelector(".holder .title").innerHTML = node.name;
         document.querySelector(".holder .url").innerHTML = node.name;
 
@@ -73,8 +79,8 @@ function updateInfo(node){
             if ( data.country_name !==  document.querySelector("#country").innerHTML ){
                 resetMap();
                 document.querySelector("#country").innerHTML = data.country_name;
-                var countryOnMap = document.querySelectorAll("svg ." + data.country_code.toLowerCase());
-                if ( countryOnMap ){ updateMap(countryOnMap, data.country_code.toLowerCase()); }
+                var countryOnMap = document.querySelectorAll("svg #" + data.country_code.toLowerCase());
+                if ( countryOnMap ){ updateMap(data.country_code.toLowerCase()); }
             }
         }
 
@@ -92,6 +98,20 @@ function updateInfo(node){
     });
 
 }
+
+
+d3.select("#mapcanvas").attr("cursor","-moz-grab").call(
+    d3.behavior.zoom()
+        .translate ([0, 0])
+        .scale (1.0)
+        .scaleExtent([1.0, 4.0])
+        .on("zoom", function(){
+            d3.selectAll("#mapcanvas > *")
+                .attr("transform","translate(" + d3.event.translate[0] + "," +  d3.event.translate[1] + ")" +
+                      " scale(" +  d3.event.scale + ")");
+        })
+);
+
 
 })(this);
 
