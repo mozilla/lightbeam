16fb40e1458b3f8bc9c355b9bf1f22cae97eb70d
Mavis Ou <mmmavisss@gmail.com>
2013-03-20 17:30:51 -0700

    removed info bar duplicated code

--- data/infobar.js
+++ data/infobar.js
@@ -3,25 +3,19 @@
 // update info
 document.querySelector('#content').addEventListener('click', function(event){
     if (event.target.mozMatchesSelector('.node')){
-        // var preHighlight = document.querySelectorAll(".highlight-country");
-//         if (preHighlight){
-//             toArray(preHighlight).forEach(function(element){
-//                 element.classList.remove("highlight-country");
-//             });
-//         }
-
         updateInfo(aggregate.nodeForKey(event.target.getAttribute('data-name')));
     }
-});
+},false);
 
-/* Updates info on the right info bar */
+// updates info on the right info bar 
 function updateInfo(node){
-    function getServerInfo(theUrl){
+
+    function getServerInfo(theUrl, callback){
         var xmlHttp = null;
         xmlHttp = new XMLHttpRequest();
         xmlHttp.open( "GET", theUrl, false );
         xmlHttp.send( null );
-        return (xmlHttp.status == 200) ? JSON.parse(xmlHttp.responseText) : false;
+        callback( (xmlHttp.status == 200) ? JSON.parse(xmlHttp.responseText) : false );
     }
 
     function resetMap(){
@@ -31,63 +25,73 @@ function updateInfo(node){
                 element.classList.remove("highlight-country");
             });
         }
-        document.querySelector("#mapcanvas").setAttribute("viewBox", [0,81.5,2711.3,1196.7].join(" "));
+        document.querySelector("#mapcanvas").setAttribute("viewBox", [0,0,2711.3,1196.7].join(" "));
     }
 
-    var nodeName = node.name;
-    document.querySelector(".holder .title").innerHTML = nodeName;
-    document.querySelector(".holder .url").innerHTML = nodeName;
-    var info = parseUri(nodeName); // uses Steven Levithan's parseUri 1.2.2
-    var jsonURL = "http://freegeoip.net/json/" + info.host;
-    var data = getServerInfo(jsonURL);
-
-    if ( data == false ){
-        document.querySelector("#country").innerHTML = "(Cannot find server location)";
-        resetMap();
-    }else{
-        // update country info only when it is different from the current one
-        if ( data.country_name !==  document.querySelector("#country").innerHTML ){
-            resetMap();
-            document.querySelector("#country").innerHTML = data.country_name;
-            var countryOnMap = document.querySelectorAll("svg ." + data.country_code.toLowerCase() + " > *");
-            if ( countryOnMap ){
-                toArray(countryOnMap).forEach(function(path){
-                    path.classList.add("highlight-country");
+    function updateMap(newCountry, countryCode){
+        toArray(newCountry).forEach(function(land){
+                    land.classList.add("highlight-country");
                 });
-            }
 
-            // position the highlighted country in center
-            var svgViewBox = document.querySelector("#mapcanvas").getAttribute("viewBox").split(" ");
-            var worldDimen = document.querySelector("#mapcanvas").getClientRects()[0];
-            var countryDimen = document.querySelector("#"+data.country_code.toLowerCase()).getClientRects()[0];
+                // position the highlighted country in center
+                var svgViewBox = document.querySelector("#mapcanvas").getAttribute("viewBox").split(" ");
+                var worldDimen = document.querySelector("#mapcanvas").getClientRects()[0];
+                var countryDimen = document.querySelector("#"+countryCode).getClientRects()[0];
 
-            var ratio = svgViewBox[2] / worldDimen.width;
-            var worldCenter = {
-                x: 0.5*worldDimen.width+worldDimen.left,
-                y: 0.5*worldDimen.height+worldDimen.top
-            };
-            var countryCenter = {
-                x: 0.5*countryDimen.width+countryDimen.left,
-                y: 0.5*countryDimen.height+countryDimen.top
-            };
-            document.querySelector("#mapcanvas").setAttribute("viewBox",
-                            [ -(worldCenter.x-countryCenter.x) * ratio,
-                              -(worldCenter.y-countryCenter.y) * ratio,
-                              svgViewBox[2],
-                              svgViewBox[3]]
-                            .join(" "));
-        }
+                var ratio = svgViewBox[2] / worldDimen.width;
+                var worldCenter = {
+                    x: 0.5*worldDimen.width + worldDimen.left,
+                    y: 0.5*worldDimen.height + worldDimen.top
+                };
+                var countryCenter = {
+                    x: 0.5*countryDimen.width + countryDimen.left,
+                    y: 0.5*countryDimen.height + countryDimen.top
+                };
+
+                var newViewBox = {
+                    x: -(worldCenter.x-countryCenter.x) * ratio,
+                    y: -(worldCenter.y-countryCenter.y) * ratio,
+                    w: svgViewBox[2],
+                    h: svgViewBox[3]
+                };
+                console.log("countryCenter = " + JSON.stringify(countryCenter));
+                console.log("newViewBox = " + JSON.stringify(newViewBox));
+                setZoom(newViewBox,'mapcanvas');
     }
 
-    // update the connections list
-    var connections = new Array();
-    var htmlList = "";
-    connections = connections.concat(node.linkedFrom, node.linkedTo);
-    connections.forEach(function(conn){
-        htmlList = htmlList + "<li>" + conn + "</li>";
+    var info = parseUri(node.name); // uses Steven Levithan's parseUri 1.2.2
+    var jsonURL = "http://freegeoip.net/json/" + info.host;
+
+    // update content in the side bar when you have the server info ==========
+    getServerInfo(jsonURL, function(data){
+        document.querySelector(".holder .title").innerHTML = node.name;
+        document.querySelector(".holder .url").innerHTML = node.name;
+
+        if ( data == false || data.country_name === "Reserved" ){
+            document.querySelector("#country").innerHTML = "(Unable to find server location)";
+            resetMap();
+        }else{
+            // update country info only when it is different from the current one
+            if ( data.country_name !==  document.querySelector("#country").innerHTML ){
+                resetMap();
+                document.querySelector("#country").innerHTML = data.country_name;
+                var countryOnMap = document.querySelectorAll("svg ." + data.country_code.toLowerCase());
+                if ( countryOnMap ){ updateMap(countryOnMap, data.country_code.toLowerCase()); }
+            }
+        }
+
+        // update the connections list
+        var connections = new Array();
+        var htmlList = "";
+        connections = connections.concat(node.linkedFrom, node.linkedTo);
+        connections.forEach(function(conn){
+            htmlList = htmlList + "<li>" + conn + "</li>";
+        });
+        document.querySelector(".connections-list").querySelector(".blue-text").innerHTML = connections.length + " connections from current site";
+        document.querySelector(".connections-list ul").innerHTML = htmlList;
+
+        document.querySelector("#content").classList.add("showinfo");
     });
-    document.querySelector(".connections-list").querySelector(".blue-text").innerHTML = connections.length + " connections from current site";
-    document.querySelector(".connections-list ul").innerHTML = htmlList;
 
 }
 
