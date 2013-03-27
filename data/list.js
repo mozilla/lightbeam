fce30cd5b16d83b4dbb4d622a088cc2bf66ad239
Mavis Ou <mmmavisss@gmail.com>
2013-03-27 10:10:13 -0700

    fixed typo

--- data/list.js
+++ data/list.js
@@ -86,7 +86,7 @@ function appendData(nodes, type, columns, filter){
         .data(function(row) {
             return columns.map(function(column, i) {
                 var value = row[i];
-                if (column==columns[1]) {value = "<span class='source-data' filter-url='"+ value +"'>" + value + "</span>";
+                if (column==columns[1]) value = "<span class='source-data' filter-url='"+ value +"'>" + value + "</span>";
                 return {column: column, value: value};
             });
         })
