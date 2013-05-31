20107bfe6fd4de1c505632727c630c64ec287adc
Mavis Ou <mmmavisss@gmail.com>
2013-05-30 17:03:23 -0700

    unused code cleanup

--- data/content-script.js
+++ data/content-script.js
@@ -1,22 +1,3 @@
-/* temp; won't need this part once we pass connection as array instead of objects */
-const SOURCE = 0;
-const TARGET = 1;
-const TIMESTAMP = 2;
-const CONTENT_TYPE = 3;
-const COOKIE = 4;
-const SOURCE_VISITED = 5;
-const SECURE = 6;
-const SOURCE_PATH_DEPTH = 7;
-const SOURCE_QUERY_DEPTH = 8;
-const SOURCE_SUB = 9;
-const TARGET_SUB = 10;
-const METHOD = 11;
-const STATUS = 12;
-const CACHEABLE = 13;
-
-/* === */
-
-
 self.port.on('log', function log(arguments){
     if (unsafeWindow && unsafeWindow.console){
         unsafeWindow.console.log.call(unsafeWindow, arguments);
@@ -39,14 +20,9 @@ self.port.on('connection', function(connection){
 self.port.on('init', function(){
     // TODO: handle(save) storage.connections
     if (unsafeWindow && unsafeWindow.currentVisualization){
-//        var connections = message.map(function(connection){
-//            connection.timestamp = new Date(connection.timestamp);
-//            return connection;
-//        });
         if ( localStorage.connections ){
             unsafeWindow.allConnections = JSON.parse(localStorage.connections);
         }
-//        unsafeWindow.currentVisualization.emit('init', connections);
         unsafeWindow.currentVisualization.emit('init', unsafeWindow.allConnections);
     }else{
         console.log('cannot call unsafeWindow.currentVisualization: ' + unsafeWindow);
