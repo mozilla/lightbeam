0aefd50311716c9d10dcd0b0be870f900a91d714
Mavis Ou <mmmavisss@gmail.com>
2013-08-09 15:53:50 -0700

    dialog - code cleanup

--- data/dialog.js
+++ data/dialog.js
@@ -23,62 +23,79 @@ function createDialog(options,callback){
         }
     });
 
-    restrictFocusWithinDialog();
-
     addDialogEventHandlers(modal,options,function(userResponse){
         callback(userResponse);
     });
 }
 
 function createDialogContent(options){
-    var titleBar = "<div class='dialog-title'>" + (options.title || "&nbsp;") + "</div>";
-    var messageBody = "<div class='dialog-message'>" + (options.message || "&nbsp;") + "</div>";
-    var content = "";
-    // dialog sign
-    var image = "";
-    if ( options.imageUrl ){
-        image = "<div class='dialog-sign'><img src='" + options.imageUrl + "' /></div>";
-    }
-    // controls
-    var controls;
-    var childElems = "";
+    return  dialogTitleBar(options) +
+            dialogMessage(options) +
+            dialogControls(options);
+}
+
+function dialogTitleBar(options){
+    return "<div class='dialog-title'>" + (options.title || "&nbsp;") + "</div>";
+}
+
+function dialogMessage(options){
+    return  "<div class='dialog-content'>" + 
+                (options.imageUrl ? "<div class='dialog-sign'><img src='" + options.imageUrl + "' /></div>" : "") + 
+                "<div class='dialog-message'>" + (options.message || "&nbsp;") + "</div>" + 
+            "</div>";
+}
+
+function dialogControls(options){
     var doNotShowAgainPrompt = "<div class='dialog-dns'><input type='checkbox' /> Do not show this again.</div>";
+    // control buttons
+    var controlButtons = "<div class='dialog-btns'>";
     var okButton = "<a class='pico-close dialog-ok'>OK</a>";
     var cancelButton = "<a class='pico-close dialog-cancel'>Cancel</a>";
-    if ( options.dnsPrompt ){ // show Do Not Show Again prompt
-        childElems += doNotShowAgainPrompt;
-    }
-    if ( options.type == "alert" ){ // alert dialog only needs a single button "OK"
-        childElems += "<div class='dialog-btns'>" + "<a class='pico-close dialog-ok'>OK</a>" + "</div>";
+    // check dialog type
+    // alert dialog only needs a single button - "OK"
+    // else we show both "OK" and "Cancel" buttons
+    if ( options.type == "alert" ){
+        controlButtons += "<a class='pico-close dialog-ok'>OK</a>";
 
     }else{
         if ( navigator.appVersion.indexOf("Win") > -1 ){ // runs on Windows
-            childElems += "<div class='dialog-btns'>" + okButton + cancelButton + "</div>";
+            controlButtons += okButton + cancelButton;
         }else{ // runs on OS other than Windows
-            childElems += "<div class='dialog-btns'>" + cancelButton + okButton + "</div>";
+            controlButtons += cancelButton + okButton;
         }
     }
+    controlButtons += "</div>";
 
-    content = "<div class='dialog-content'>" + image + messageBody + "</div>";
-    controls = "<div class='dialog-controls'>" + childElems + "</div>";
-
-    return titleBar + content + controls;
-}
-
-function restrictFocusWithinDialog(){
-    var allElemsInDialog = document.querySelectorAll(".pico-content *");
-    var tabIndex = 0;
-    toArray(allElemsInDialog).forEach(function(elem, i){
-        if ( elem.nodeName.toLowerCase() == "a" || elem.nodeName.toLowerCase() == "input" ){
-            elem.setAttribute("tabIndex", tabIndex);
-            tabIndex++;
-        }
-    });
-    document.querySelector(".pico-content [tabIndex='0']").focus();
+    return  "<div class='dialog-controls'>" + 
+                ( options.dnsPrompt ? doNotShowAgainPrompt : "" ) +
+                controlButtons + 
+            "</div>";
 }
 
 function addDialogEventHandlers(modal,options,callback){
-    var keyPressHandler = function(event){
+    var dialogContainer = modal.modalElem;
+    // OK button click event handler
+    var okButton = dialogContainer.querySelector(".pico-close.dialog-ok");
+    okButton.addEventListener("click",function(){
+        if ( dialogContainer.querySelector(".dialog-dns input") && dialogContainer.querySelector(".dialog-dns input").checked ){ // Do Not Show
+            var dnsPref = localStorage.dnsDialogs || "[]";
+            dnsPref = JSON.parse(dnsPref);
+            dnsPref.push(options.name);
+            localStorage.dnsDialogs = JSON.stringify(dnsPref);
+        }
+        modal.close();
+        callback(true);
+    });
+    // Cancel button click event handler
+    var cancelButton = dialogContainer.querySelector(".pico-close.dialog-cancel");
+    if ( cancelButton ){
+        cancelButton.addEventListener("click",function(){
+            modal.close();
+            callback(false);
+        });
+    }
+
+    var keyDownHandler = function(event){
         // disable Tab
         if ( event.keyCode == "9" ){ 
             event.preventDefault();
@@ -89,21 +106,29 @@ function addDialogEventHandlers(modal,options,callback){
             callback(false);
         }
     }
+    document.addEventListener("keydown", keyDownHandler);
 
-    document.addEventListener("keydown", keyPressHandler);
+    modal.onClose(function(){
+        document.removeEventListener("keydown", keyDownHandler);
+    });
 
-    // restrict Tab/focus within dialogs
-    document.querySelector(".pico-content").addEventListener("keypress", function(event){
+    restrictTabWithinDialog(modal);
+}
+
+function restrictTabWithinDialog(modal){
+    var dialogContainer = modal.modalElem;
+    assignTabIndices(modal);
+    dialogContainer.addEventListener("keypress", function(event){
         event.stopPropagation();
         var focusedElm = document.activeElement;
         // Tab key is pressed
         if ( event.keyCode == "9" ){
             var currentTabIndex = parseInt(focusedElm.getAttribute("tabIndex"));
-            var nextElem = document.querySelector(".pico-content [tabIndex='" + (currentTabIndex+1) + "']");
+            var nextElem = dialogContainer.querySelector("[tabIndex='" + (currentTabIndex+1) + "']");
             if ( nextElem ){
                 nextElem.focus();
             }else{
-                document.querySelector(".pico-content [tabIndex='0']").focus();
+                dialogContainer.querySelector("[tabIndex='0']").focus();
             }
         }
         // when the focused element is the OK or Cancel button and Enter key is pressed
@@ -112,30 +137,17 @@ function addDialogEventHandlers(modal,options,callback){
             focusedElm.click();
         }
     });
+}
 
-    modal.onClose(function(){
-        document.removeEventListener("keydown", keyPressHandler);
-    });
-    
-    // OK button click event handler
-    var okButton = document.querySelector(".pico-close.dialog-ok");
-    document.querySelector(".pico-close.dialog-ok").addEventListener("click",function(){
-        if ( document.querySelector(".dialog-dns input") && document.querySelector(".dialog-dns input").checked ){ // Do Not Show
-            var dnsPref = localStorage.dnsDialogs || "[]";
-            dnsPref = JSON.parse(dnsPref);
-            dnsPref.push(options.name);
-            localStorage.dnsDialogs = JSON.stringify(dnsPref);
+function assignTabIndices(modal){
+    var dialogContainer = modal.modalElem;
+    var allElemsInDialog = dialogContainer.querySelectorAll("*");
+    var tabIndex = 0;
+    toArray(allElemsInDialog).forEach(function(elem, i){
+        if ( elem.nodeName.toLowerCase() == "a" || elem.nodeName.toLowerCase() == "input" ){
+            elem.setAttribute("tabIndex", tabIndex);
+            tabIndex++;
         }
-        modal.close();
-        callback(true);
     });
-
-    // Cancel button click event handler
-    var cancelButton = document.querySelector(".pico-close.dialog-cancel");
-    if ( cancelButton ){
-        cancelButton.addEventListener("click",function(){
-            modal.close();
-            callback(false);
-        });
-    }
+    dialogContainer.querySelector("[tabIndex='0']").focus();
 }
