39ac7f5d792fee05b5825e68737774285a74f4ce
dethe <dethe@livingcode.org>
2013-10-16 21:24:16 -0700

    fix content and add staging server url

--- data/collusion.js
+++ data/collusion.js
@@ -12,7 +12,7 @@ try{
     userSettings = {};
 }
 // FIXME: Read this from config file
-var uploadServer = 'http://collusiondb-development.herokuapp.com/shareData';
+var uploadServer = 'http://collusiondb.mofostaging.net/shareData';
 var isRobot = false; // Used for spidering the web only
 var uploadTimer;
 var saveTimer;
