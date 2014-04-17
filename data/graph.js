mozilla/lightbeam

commit 8256f234d828c0f2f7d97695d3413d6cd5b920eb

Address some of Garrett's comments

data/graph.js
@@ -127,8 +127,8 @@ function nodeName(node){
     }
     return undefined;
 }
-global.siteHasPref = function siteHasPref(site,pref){
-  return (Object.keys(userSettings).indexOf(site) > -1 &&
+function siteHasPref(site,pref){
+  return (userSettings.hasOwnProperty(pref) &&
           userSettings[site].contains(pref));
 }
 
@@ -145,7 +145,7 @@ var ticking = false;
 
 function charge(d){ return -(500 +  d.weight * 25); }
 
-global.colourHighlightNodes = function colourHighlightNodes(highlight){
+function colourHighlightNodes(highlight){
     var watchedSites = document.querySelectorAll(".watched");
     var blockedSites = document.querySelectorAll(".blocked");
     if ( highlight.watched ){
@@ -334,9 +334,19 @@ function resetCanvas(){
 
 var graphLegend = document.querySelector(".graph-footer");
 
-if (global.legendBtnClickHandler) {
-  global.legendBtnClickHandler(graphLegend);
+function legendBtnClickHandler(legendElm){
+    legendElm.querySelector(".legend-controls").addEventListener("click", function(event){
+        if (event.target.mozMatchesSelector(".btn, .btn *")){
+            var btn = event.target;
+            while(btn.mozMatchesSelector('.btn *')){
+                btn = btn.parentElement;
+            }
+            btn.classList.toggle("active");
+        }
+    });
 }
+
+legendBtnClickHandler(graphLegend);
 
 graphLegend.querySelector(".legend-toggle-visited").addEventListener("click", function(event){
     var visited = document.querySelectorAll(".visitedYes");

data/aggregate.js
@@ -133,10 +133,8 @@ aggregate.connectionAsObject = function(conn){
 // visualizations.
 function onLoad(connections){
     var startTime = Date.now();
-    if (connections) {
-      console.log("aggregate::onLoad", connections.length, "connections", aggregate.currentFilter);
-      connections.forEach(onConnection);
-    }
+    console.log("aggregate::onLoad", connections.length, "connections", aggregate.currentFilter);
+    connections.forEach(onConnection);
     aggregate.initialized = true;
     filteredAggregate = aggregate.filters[aggregate.currentFilter]();
 

data/infobar.js
@@ -252,7 +252,7 @@ document.querySelector(".toggle-site-profile").addEventListener("click", functio
 /* Toggle Help Sections */
 document.querySelector(".toggle-help").addEventListener("click", function(){
     var tabClicked = document.querySelector(".toggle-help");
-    var contentToBeShown = document.querySelector(".help-content ." + currentVisualization.name +"-view-help");
+    var contentToBeShown = document.querySelector(".help-content ." + global.currentVisualization + "-view-help");
     toggleInfoPanelTab(tabClicked, contentToBeShown);
 });
 

data/ui.js
@@ -317,18 +317,6 @@ global.toggleVizElements = function toggleVizElements(elements,classToggle){
     });
 }
 
-global.legendBtnClickHandler = function legendBtnClickHandler(legendElm){
-    legendElm.querySelector(".legend-controls").addEventListener("click", function(event){
-        if (event.target.mozMatchesSelector(".btn, .btn *")){
-            var btn = event.target;
-            while(btn.mozMatchesSelector('.btn *')){
-                btn = btn.parentElement;
-            }
-            btn.classList.toggle("active");
-        }
-    });
-}
-
 
 
 /* Glowing Effect for Graph/Clock & Highlighting Effect for List ============= */
@@ -456,10 +444,11 @@ function updateUIFromPrefs(event) {
   // This is not working quite
   if ("defaultFilter" in event) {
     aggregate.currentFilter = event["defaultFilter"];
-    document.querySelector('a[data-value=' + aggregate.currentFilter + ']').
-      dataset.selected = true;
+    document.querySelector('a[data-value=' + aggregate.currentFilter + ']')
+            .dataset.selected = true;
     document.querySelector(".filter-display header").textContent =
-      document.querySelector(".btn_group.session").querySelector("[data-selected]").textContent;
+      document.querySelector(".btn_group.session")
+              .querySelector("[data-selected]").textContent;
   }
 }
 

commit c958002e7d8304ec8d7c6ed90844381c10aaa309

Restore coloured triangles for blocked sites (fix #545)
This bug was introduced in 8256f234d828c0f2f7d97695d3413d6cd5b920eb.

data/graph.js
@@ -128,7 +128,7 @@ function nodeName(node){
     return undefined;
 }
 function siteHasPref(site,pref){
-  return (userSettings.hasOwnProperty(pref) &&
+  return (userSettings.hasOwnProperty(site) &&
           userSettings[site].contains(pref));
 }
 