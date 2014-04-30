2e526e36d56923c4b79fe3b7b06b30516d7bd48b
Francois Marier <francois@mozilla.com>
2014-04-30 18:45:12 +1200

    Add MPL copyright header to all of our files
    As per https://www.mozilla.org/MPL/headers/

--- data/content-script.js
+++ data/content-script.js
@@ -1,3 +1,6 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 (function (global) {
 
 // This is the e10s/message passing content script that ties the workers to the
--- data/events.js
+++ data/events.js
@@ -1,3 +1,6 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 // Basic implementation of an event emitter for visualization plugins
 // This may be built-in to jetpack, but it's not available on the HTML side so
 // we need to keep this.
